6106a4e4f0741eb94da0f4fa99726176460b916e
saipreetibalaji <saipreetibalaji@gmail.com>
2024-11-29 19:00:42 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int radius;
+int area;
+printf("radius of circle:\n");
+scanf("%d",&radius);
+area=3.14*(radius*radius);
+printf("radius is:%d\n",radius);
+printf("area is:%d\n",area);
+}
